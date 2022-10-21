2d314b6a2ebc5fa3134173cbbe6ebb0ae79df9b3
Kevin Old <kevin@kevinold.com>
2022-10-21 11:19:07 -0500

    attempt to load redirects from aws-exports.js

--- amplify/backend/auth/202210061348amplifycae24809c/override.ts
+++ amplify/backend/auth/202210061348amplifycae24809c/override.ts
@@ -1,4 +1,5 @@
 import { AmplifyAuthCognitoStackTemplate } from '@aws-amplify/cli-extensibility-helper';
+//const awsconfig = require(path.join(__dirname, "../../../../../src/aws-exports.js"));
 
 const allowedOAuthScopes = ["aws.cognito.signin.user.admin", "email", "openid", "phone", "profile"]
 
@@ -9,14 +10,18 @@ export function override(resources: AmplifyAuthCognitoStackTemplate) {
     const { userPoolClient, userPoolClientWeb } = resources
 
     userPoolClient.addPropertyOverride("SupportedIdentityProviders", ["Okta"])
+    // ideally from  awsconfig.oauth.redirectSignIn
     userPoolClient.addPropertyOverride("CallbackURLs", ["http://localhost:3000/"])
+    // ideally from  awsconfig.oauth.redirectSignOut
     userPoolClient.addPropertyOverride("LogoutURLs", ["http://localhost:3000/"])
     userPoolClient.addPropertyOverride("AllowedOAuthFlows", ["code"])
     userPoolClient.addPropertyOverride("AllowedOAuthFlowsUserPoolClient", "true")
     userPoolClient.addPropertyOverride("AllowedOAuthScopes", allowedOAuthScopes)
 
     userPoolClientWeb.addPropertyOverride("SupportedIdentityProviders", ["Okta"])
+    // ideally from  awsconfig.oauth.redirectSignIn
     userPoolClientWeb.addPropertyOverride("CallbackURLs", ["http://localhost:3000/"])
+    // ideally from  awsconfig.oauth.redirectSignOut
     userPoolClientWeb.addPropertyOverride("LogoutURLs", ["http://localhost:3000/"])
     userPoolClientWeb.addPropertyOverride("AllowedOAuthFlows", ["code"])
     userPoolClientWeb.addPropertyOverride("AllowedOAuthFlowsUserPoolClient", "true")
